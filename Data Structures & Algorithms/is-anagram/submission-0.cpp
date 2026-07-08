f3557b207acf48e6cf92b156364aa8cf41adb914
bhargavdahake29 <ramuji.bhargav24@vit.edu>
2026-07-08 19:09:28 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!= t.length()){
+            return false;
+        }
+        vector<int> freq(26, 0);
+
+        for(auto c: s){
+            freq[c-'a']++;
+        }
+
+        for(auto c: t){
+            freq[c-'a']--;
+        }
+
+        for(int x: freq){
+            if(x!=0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
