d35b98bb3f466ff0d6c75423beebe46aa9d01107
bhargavdahake29 <ramuji.bhargav24@vit.edu>
2026-07-10 22:25:02 +0530

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        sort(points.begin(), points.end(), [](auto& a, auto& b) {
+            int d1 = a[0] * a[0] + a[1] * a[1];
+            int d2 = b[0] * b[0] + b[1] * b[1];
+
+            return d1 < d2;
+        });
+
+        vector<vector<int>> ans;
+
+        for(int i=0; i<k; i++){
+            ans.push_back(points[i]);
+        }
+
+        return ans;
+    }
+};
